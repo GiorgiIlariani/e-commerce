877c93d5072c009d67d8de9c0447b4c30b6077c7
GiorgiIlariani <lmaisuradze3333@gmail.com>
2024-03-06 22:32:10 +0400

    changes searchComponent structure

--- components/shared/Search.tsx
+++ components/shared/Search.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { IoSearch } from "react-icons/io5";
@@ -16,40 +16,38 @@ const SearchComponent = () => {
     setQuery(e.target.value);
   };
 
-  useEffect(() => {
-    const delayDebounceFn = setTimeout(() => {
-      let newUrl = "";
+  const handleClick = () => {
+    let newUrl = "";
 
-      if (query) {
-        newUrl = formUrlQuery({
-          params: searchParams.toString(),
-          key: "query",
-          value: query,
-        });
-      } else {
-        newUrl = removeKeysFromQuery({
-          params: searchParams.toString(),
-          keysToRemove: ["query"],
-        });
-      }
+    if (query) {
+      newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "query",
+        value: query,
+      });
+    } else {
+      newUrl = removeKeysFromQuery({
+        params: searchParams.toString(),
+        keysToRemove: ["query"],
+      });
+    }
 
-      router.push(newUrl, { scroll: false });
-    }, 300);
-
-    return () => clearTimeout(delayDebounceFn);
-  }, [query, router, searchParams]);
+    router.push(newUrl, { scroll: false });
+  };
 
   return (
     <section className="wrapper">
       <h2 className="font-medium text-2xl">Everything you're looking for</h2>
       <div className="flex items-center gap-5 pt-6">
         <Input
-          placeholder="მაგ.Iphone 14"
+          placeholder="iphone 14"
           value={query}
           onChange={changeHandler}
           className="py-6 px-4 outline-none rounded-2xl border border-[#fec900] ring-none"
         />
-        <Button className="text-white bg-[#fec900] min-w-[180px] rounded-2xl px-[14px] py-6 flex items-center gap-3 text-base">
+        <Button
+          className="text-white bg-[#fec900] min-w-[180px] rounded-2xl px-[14px] py-6 flex items-center gap-3 text-base"
+          onClick={handleClick}>
           <IoSearch />
           Search
         </Button>
