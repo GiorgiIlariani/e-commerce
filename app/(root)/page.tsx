b486356212158fa2b9479a6f4134d981d8195eb2
GiorgiIlariani <lmaisuradze3333@gmail.com>
2024-03-07 17:57:04 +0400

    same loading vhunk problem

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,7 @@ import SearchComponent from "@/components/shared/Search";
 const HomePage = () => {
   return (
     <div className="min-h-[200vh] flex flex-col mt-8">
-      <SearchComponent />
+      {/* <SearchComponent /> */}
     </div>
   );
 };
