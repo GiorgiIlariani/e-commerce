6f154049b86eb40654489fa6526455a093645a0c
GiorgiIlariani <lmaisuradze3333@gmail.com>
2024-03-07 18:43:52 +0400

    test

--- components/shared/Search.tsx
+++ components/shared/Search.tsx
@@ -17,24 +17,24 @@ const SearchComponent = () => {
     setQuery(e.target.value);
   };
 
-  const handleClick = () => {
-    let newUrl = "";
+  // const handleClick = () => {
+  //   let newUrl = "";
 
-    if (query) {
-      newUrl = formUrlQuery({
-        params: searchParams.toString(),
-        key: "query",
-        value: query,
-      });
-    } else {
-      newUrl = removeKeysFromQuery({
-        params: searchParams.toString(),
-        keysToRemove: ["query"],
-      });
-    }
+  //   if (query) {
+  //     newUrl = formUrlQuery({
+  //       params: searchParams.toString(),
+  //       key: "query",
+  //       value: query,
+  //     });
+  //   } else {
+  //     newUrl = removeKeysFromQuery({
+  //       params: searchParams.toString(),
+  //       keysToRemove: ["query"],
+  //     });
+  //   }
 
-    router.push(newUrl, { scroll: false });
-  };
+  //   router.push(newUrl, { scroll: false });
+  // };
 
   return (
     <section className="wrapper">
@@ -42,18 +42,19 @@ const SearchComponent = () => {
         Everything you're looking for
       </h2>
       <div className="flex items-center gap-4 pt-6">
-        {/* <Input
+        <Input
           placeholder="iphone 14"
           value={query}
           onChange={changeHandler}
           className="h-[50px] px-4 outline-none rounded-2xl border border-[#fec900] ring-none"
-        /> */}
+        />
         <div className="min-w-[68px] h-[50px] rounded-2xl bg-[#f1f3f6] hidden md:flex items-center justify-center cursor-pointer">
           <VscSettings />
         </div>
         <Button
           className="h-[50px] text-white bg-[#fec900] min-w-[180px] rounded-2xl px-[14px] items-center gap-3 text-base  hidden md:flex"
-          onClick={handleClick}>
+          // onClick={handleClick}
+        >
           <IoSearch />
           Search
         </Button>
