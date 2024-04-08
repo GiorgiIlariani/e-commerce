46049bbe0d06fabfe5d6fe83e42b334e26fc0d4d
GiorgiIlariani <lmaisuradze3333@gmail.com>
2024-04-08 20:38:19 +0400

    trying to deploy web site

--- app/(root)/(userActivity)/product-form/page.tsx
+++ app/(root)/(userActivity)/product-form/page.tsx
@@ -3,8 +3,8 @@ import { fetchCategoriesList } from "@/lib/actions/categories-actions";
 import React from "react";
 
 const ProductFormPage = async () => {
-  const categories = await fetchCategoriesList();
-  console.log({ categories });
+  // const categories = await fetchCategoriesList();
+  // console.log({ categories });
 
   return <ProductForm />;
 };
